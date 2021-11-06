3ebd478166c7bcaf5496d29dc4f4f8d258899e6a
rsramirez4 <93182347+rsramirez4@users.noreply.github.com>
2021-11-05 18:59:23 -0600

    Add files via upload

---
+++ bin/QTFILEOPEN&READ/mainwindow.h
@@ -0,0 +1,41 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QFile>        //lets us open files
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    int currentCount = 0;
+    int output = 0; // Output Register
+    int input = 0;  // Input Register
+    int opcode;
+    int accumulator = 0;
+    int ROM [16] = {0b10000000, 0b00110001, 0b10000000, 0b00010001,0b10010000,0b01010000,0,0,0,0,0,0,0,0,0,0};
+    int Ram [16];
+    int data = 0;
+    void InstructionDecoder();
+
+    QString currentFile = "";   //lets us open files
+private slots: // executed when signal is submitted
+    void CLOCKButtonPressed();
+
+    void on_actionOpen_triggered();
+};
+#endif // MAINWINDOW_H
