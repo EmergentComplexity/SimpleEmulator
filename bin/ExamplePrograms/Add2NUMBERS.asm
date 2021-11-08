59f3da0e9bf6c84950f20e273b8be14522f18437
Peter Raphael Sanchez <84114155+EmergentComplexity@users.noreply.github.com>
2021-11-07 21:43:18 -0700

    Update Add2NUMBERS.asm
    it likes single spaces

--- bin/ExamplePrograms/Add2NUMBERS.asm
+++ bin/ExamplePrograms/Add2NUMBERS.asm
@@ -3,4 +3,4 @@ STO 0001 ; Store the value inputed to address 1
 INP 0000 ; Input another value
 ADD 0001 ; Sum of 2 inputed values, the first value entered is in address 1 and the second is still in the accumulator
 OUT 0000 ; output the sum, the accumulator is written to the output register, 0000 because OUT doesnt need an operand
-B   0000 ; unconditional jump to start of program, add 2 more numbers
+B 0000 ; unconditional jump to start of program, add 2 more numbers
